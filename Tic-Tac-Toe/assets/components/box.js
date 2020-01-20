d7485c3bb13bc2b045e611cf9f8836225d885762
María Beltrán <mariabeltra.98@gmail.com>
2020-01-20 11:28:01 -0300

    maqueteando la primera casilla

---
+++ Tic-Tac-Toe/assets/components/box.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+export default class Box extends Component {
+    constructor(){
+        super()
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(evt) {
+        this.props.onClick(this.props.rowNum, this.props.colNum);
+      }
+    render(){
+     
+      return (
+        <View style={styles.container_Home}>
+          
+      <TouchableOpacity
+      onPress={this.handleClick}
+      style={[styles.option]}
+      disabled={this.props.buttonValue !== null || this.props.gameOver}>
+          <Text style={[styles.optionText]}>
+          {this.props.buttonValue ? this.props.buttonValue : ' '}
+        </Text>
+      </TouchableOpacity>
+     
+        </View>
+      );
+    }
+    }
+    Box.propTypes = {
+        buttonValue: PropTypes.string,
+        gameOver: PropTypes.bool,
+        rowNum: PropTypes.number,
+        colNum: PropTypes.number,
+        onClick: PropTypes.func
+      };
+      
+      const styles = StyleSheet.create({
+        option: {
+          flex: 1,
+          backgroundColor: 'transparent',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderWidth: 1,
+          borderColor: 'black'
+        },
+      
+        optionText: {
+          fontSize: 52,
+          fontWeight: '900'
+        }
+      });
--- Tic-Tac-Toe/assets/views/home.js
+++ Tic-Tac-Toe/assets/views/home.js
@@ -1,31 +1,23 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import GameMode from '../components/GameMode';
-import shield from '../images/shield.png'
-import sword from '../images/swords.png'
+import Box from '../components/box';
+
 
 export default class Home extends Component {
   constructor(){
     super()
     this.state={
-      token: "",
-      chooseToken: [shield, sword]
+    
     }
-  //  this.tokenChoice = this.tokenChoice.bind(this);
+  
   }
-tokenChoice(e){
-    console.log(shield)
-    alert(shield)
 
-}
   render(){
     return (
       <View style={styles.container_Home}>
         
     <Text>Jugemos!!!</Text>
-    <GameMode
-    img={this.state.chooseToken}
-    tokenChoice={(e)=>this.tokenChoice(e)}/>
+   <Box/>
       </View>
     );
   }
